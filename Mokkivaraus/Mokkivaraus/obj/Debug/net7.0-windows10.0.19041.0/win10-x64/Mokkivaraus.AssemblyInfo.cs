87e508d7b8492619bf325e1b407a25aef7702aa1
antisankari <anssi.sankari@gmail.com>
2024-04-06 16:15:45 +0300

    AS muutostesti

--- Mokkivaraus/Mokkivaraus/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/Mokkivaraus.AssemblyInfo.cs
+++ Mokkivaraus/Mokkivaraus/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/Mokkivaraus.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mokkivaraus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+23cadd663e3ab3921fe612f584990f8539944707")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mokkivaraus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mokkivaraus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
