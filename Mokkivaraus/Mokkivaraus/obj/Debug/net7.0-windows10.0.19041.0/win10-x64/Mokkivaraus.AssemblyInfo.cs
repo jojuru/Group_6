439d4dbaae734d542d03f44d99d833b5ba7f8862
jojuru <joonaruusu@gmail.com>
2024-04-06 15:51:07 +0300

    on

--- Mokkivaraus/Mokkivaraus/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/Mokkivaraus.AssemblyInfo.cs
+++ Mokkivaraus/Mokkivaraus/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/Mokkivaraus.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mokkivaraus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+577ca4b95d43fb1bf2174295fff119b525f79e98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mokkivaraus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mokkivaraus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
