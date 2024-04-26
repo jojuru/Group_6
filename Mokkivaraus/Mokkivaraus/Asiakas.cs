5023bf54b55e3a9947a3bf8f197ceac5e611c844
jojuru <joonaruusu@gmail.com>
2024-04-27 01:35:49 +0300

    27.4. Lisäykset

---
+++ Mokkivaraus/Mokkivaraus/Asiakas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mokkivaraus
+{
+    public class Asiakas
+    {
+        public string asiakas_id { get; set; }
+        public string postinro { get; set; }
+        public string etunimi { get; set; }
+        public string sukunimi { get; set; }
+        public string lahiosoite { get; set; }
+        public string email { get; set; }
+        public string puhelinnro { get; set; }
+
+        public Asiakas() { }
+        public Asiakas(string asiakas_id, string postinro, string etunimi, 
+            string sukunimi, string lahiosoite, string email, string puhelinnro)
+        {
+            this.asiakas_id = asiakas_id;
+            this.postinro = postinro;
+            this.etunimi = etunimi;
+            this.sukunimi = sukunimi;
+            this.lahiosoite = lahiosoite;
+            this.email = email;
+            this.puhelinnro = puhelinnro;
+        }
+    }
+}
--- Mokkivaraus/Mokkivaraus/HallintaPage.xaml.cs
+++ Mokkivaraus/Mokkivaraus/HallintaPage.xaml.cs
@@ -6,21 +6,37 @@ namespace Mokkivaraus;
 public partial class HallintaPage : TabbedPage
 {
     public ObservableCollection<Alue> AlueCollection { get; set; }
+    public ObservableCollection<Asiakas> AsiakasCollection { get; set; }
+
+    static private String connstring = "server=localhost;uid=root;pwd=root;database=vn";
     public HallintaPage()
 	{
 		InitializeComponent();
         AlueCollection = new ObservableCollection<Alue>();
-		BindingContext = this;
+        AsiakasCollection = new ObservableCollection<Asiakas>();
+        BindingContext = this;
 		AlueListaLv.BindingContext = AlueCollection;
+        AsiakasListaLv.BindingContext = AsiakasCollection;
 
 
-        SqlHaku();
+        AlueCollection = new ObservableCollection<Alue>();
+        BindingContext = this;
+        AlueListaLv.BindingContext = AlueCollection;
+
+        SqlHaeKaikki();
     }
-	
-	//hakee tietokannastta kaiken
-	private void SqlHaku()
+
+    //hakee tietokannasta kaikki
+    private void SqlHaeKaikki()
 	{
-		string connstring = "server=localhost;uid=root;pwd=root;database=vn";
+        SqlHaeAsiakkaat();
+        SqlHaeAlueet();
+
+	}
+	
+	//hakee tietokannastta Alueet
+	private void SqlHaeAlueet()
+	{
 		MySqlConnection con = new MySqlConnection();
 		con.ConnectionString = connstring;
 		con.Open();
@@ -38,4 +54,51 @@ public partial class HallintaPage : TabbedPage
 		AlueListaLv.ItemsSource = AlueCollection;
 	}
 
+    //hakee tietokannastta Asiakkaat
+    private void SqlHaeAsiakkaat()
+	{
+        MySqlConnection con = new MySqlConnection();
+        con.ConnectionString = connstring;
+        con.Open();
+        string sql = "SELECT * FROM asiakas";
+        MySqlCommand cmd = new MySqlCommand(sql, con);
+        MySqlDataReader reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Asiakas ASIAKAS  = new Asiakas();
+            ASIAKAS.asiakas_id = reader["asiakas_id"].ToString();
+            ASIAKAS.etunimi = reader["etunimi"].ToString();
+            ASIAKAS.sukunimi = reader["sukunimi"].ToString();
+            ASIAKAS.postinro = reader["postinro"].ToString();
+            ASIAKAS.lahiosoite = reader["lahiosoite"].ToString();
+            ASIAKAS.email = reader["email"].ToString();
+            ASIAKAS.puhelinnro = reader["puhelinnro"].ToString();
+            AsiakasCollection.Add(ASIAKAS);
+        }
+        AsiakasListaLv.ItemsSource = AsiakasCollection;
+    }
+
+    //Hakee alueita alkean ensimmäisestä kirjaimesta
+    private void AlueHaeBtn_Clicked(object sender, EventArgs e)
+    {
+        AlueCollection.Clear();
+        AlueListaLv.ItemsSource = AlueCollection;
+
+        MySqlConnection con = new MySqlConnection();
+        con.ConnectionString = connstring;
+        con.Open();
+        string sql = "SELECT * FROM alue WHERE nimi LIKE '" 
+            + AlueNimiEnt.Text + "%'";
+        MySqlCommand cmd = new MySqlCommand(sql, con);
+        MySqlDataReader reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            Alue ALUE = new Alue();
+            ALUE.alue_id = reader["alue_id"].ToString();
+            ALUE.nimi = reader["nimi"].ToString();
+            AlueCollection.Add(ALUE);
+        }
+        AlueListaLv.ItemsSource = AlueCollection;
+    }
 }
