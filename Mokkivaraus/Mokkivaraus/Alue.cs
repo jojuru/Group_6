8f2dc7fd5db7b3668ee83cbf3ee68f7a6cefcc86
jojuru <joonaruusu@gmail.com>
2024-04-26 21:12:59 +0300

    Alue alku haku

---
+++ Mokkivaraus/Mokkivaraus/Alue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mokkivaraus
+{
+    public class Alue
+    {
+        public string alue_id { get; set; }
+        public string nimi { get; set; }
+    
+        public Alue() { }
+        public Alue(string alue_id, string nimi)
+        {
+            this.alue_id = alue_id;
+            this.nimi = nimi;
+        }
+    }
+}
--- Mokkivaraus/Mokkivaraus/HallintaPage.xaml.cs
+++ Mokkivaraus/Mokkivaraus/HallintaPage.xaml.cs
@@ -1,11 +1,41 @@
+using MySql.Data.MySqlClient;
+using System.Collections.ObjectModel;
+
 namespace Mokkivaraus;
 
 public partial class HallintaPage : TabbedPage
 {
-	public HallintaPage()
+    public ObservableCollection<Alue> AlueCollection { get; set; }
+    public HallintaPage()
 	{
 		InitializeComponent();
-	}
+        AlueCollection = new ObservableCollection<Alue>();
+		BindingContext = this;
+		AlueListaLv.BindingContext = AlueCollection;
 
+
+        SqlHaku();
+    }
+	
+	//hakee tietokannastta kaiken
+	private void SqlHaku()
+	{
+		string connstring = "server=localhost;uid=root;pwd=root;database=vn";
+		MySqlConnection con = new MySqlConnection();
+		con.ConnectionString = connstring;
+		con.Open();
+		string sql = "SELECT * FROM alue";
+		MySqlCommand cmd = new MySqlCommand(sql, con);
+		MySqlDataReader reader = cmd.ExecuteReader();
+
+		while (reader.Read())
+		{
+            Alue ALUE = new Alue();
+			ALUE.alue_id = reader["alue_id"].ToString();
+            ALUE.nimi = reader["nimi"].ToString();
+			AlueCollection.Add(ALUE);
+        }
+		AlueListaLv.ItemsSource = AlueCollection;
+	}
 
 }
