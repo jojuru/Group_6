f360d98df81939bf74d383cd7aa5aceb514f1da8
jojuru <joonaruusu@gmail.com>
2024-04-26 18:13:03 +0300

    Hallinta pohja

--- Mokkivaraus/Mokkivaraus/obj/Debug/net7.0-maccatalyst/maccatalyst-x64/Mokkivaraus.AssemblyInfo.cs
+++ Mokkivaraus/Mokkivaraus/obj/Debug/net7.0-maccatalyst/maccatalyst-x64/Mokkivaraus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mokkivaraus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f04365e630c8543c6887b38dc738229be83869a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mokkivaraus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mokkivaraus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
