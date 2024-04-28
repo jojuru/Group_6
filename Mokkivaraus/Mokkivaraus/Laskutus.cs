25f97af59e8cfe873d21245610ddd97b992bf50e
antisankari <anssi.sankari@gmail.com>
2024-04-28 19:12:32 +0300

    laskutus.cs luotu, laskulista tuotu nakyviin

---
+++ Mokkivaraus/Mokkivaraus/Laskutus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mokkivaraus
+{
+    public class Laskutus
+    {
+        public string lasku_id { get; set; }
+        public string varaus_id { get; set; }
+        public string summa { get; set; }
+        public string alv { get; set; }
+        public string maksettu { get; set; }
+
+        public Laskutus() { }
+
+        public Laskutus(string lasku_id, string varaus_id, string summa, string alv, string alue, string maksettu)
+        {
+            this.lasku_id = lasku_id;
+            this.varaus_id = varaus_id;
+            this.summa = summa;
+            this.alv = alv;
+            this.maksettu = maksettu;
+        }
+    }
+}
--- Mokkivaraus/Mokkivaraus/LaskutusPage.xaml.cs
+++ Mokkivaraus/Mokkivaraus/LaskutusPage.xaml.cs
@@ -1,9 +1,42 @@
+using MySql.Data.MySqlClient;
+using System.Collections.ObjectModel;
 namespace Mokkivaraus;
 
 public partial class LaskutusPage : TabbedPage
-{
-	public LaskutusPage()
+{	
+	public ObservableCollection<Laskutus> LaskutusCollection { get; set; }
+
+    static private String connstring = "server=localhost;uid=root;port=3306;pwd=Verorakoja123;database=vn";
+    public LaskutusPage()
 	{
 		InitializeComponent();
+
+        LaskutusCollection = new ObservableCollection<Laskutus>();
+        LaskuListaLv.BindingContext = LaskutusCollection;
+        
+        SqlHaeLaskut();
 	}
+
+	private void SqlHaeLaskut()
+	{
+        MySqlConnection con = new MySqlConnection();
+        con.ConnectionString = connstring;
+        con.Open();
+        string sql = "SELECT * FROM lasku";
+        MySqlCommand cmd = new MySqlCommand(sql, con);
+        MySqlDataReader reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Laskutus LASKUTUS = new Laskutus();
+            LASKUTUS.lasku_id = reader["lasku_id"].ToString();
+            LASKUTUS.varaus_id = reader["varaus_id"].ToString();
+            LASKUTUS.summa = reader["summa"].ToString();
+            LASKUTUS.alv = reader["alv"].ToString();
+            LASKUTUS.maksettu = reader["maksettu"].ToString();
+            LaskutusCollection.Add(LASKUTUS);
+        }
+        LaskuListaLv.ItemsSource = LaskutusCollection;
+    }
+
 }
